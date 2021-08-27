85c589166a145147179e8bab2ec786e5b305cb0a
LSimoncelio <simoncelio1965@gmail.com>
2021-08-27 12:23:05 -0300

    Commit inicial

---
+++ src/main/kotlin/one/digitalinonovation/dionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinonovation.dionebank
+
+class Pessoa {
+    var nome: String = "Lucas"
+    var cpf: String = "123.123.123-12"
+}
+
+fun main() {
+    val lucas = Pessoa()
+
+    println(lucas.nome)
+    println(lucas.cpf)
+}
