b779a7cd552acf7610b84242b48b8f610f799a48
LSimoncelio <simoncelio1965@gmail.com>
2021-08-27 12:56:20 -0300

    update variavel private e membros da classe

--- src/main/kotlin/one/digitalinonovation/dionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinonovation/dionebank/Pessoa.kt
@@ -3,11 +3,16 @@ package one.digitalinonovation.dionebank
 class Pessoa {
     var nome: String = "Lucas"
     var cpf: String = "123.123.123-12"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val lucas = Pessoa()
 
-    println(lucas.nome)
-    println(lucas.cpf)
+    println(lucas)
+    println(lucas.pessoaInfo())
 }
