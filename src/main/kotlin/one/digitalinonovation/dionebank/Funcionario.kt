90bbbea221e7fa45fa593e03c74be3013de4847c
LSimoncelio <simoncelio1965@gmail.com>
2021-08-27 21:14:35 -0300

    atualização de cod para abstract

---
+++ src/main/kotlin/one/digitalinonovation/dionebank/Funcionario.kt
@@ -0,0 +1,11 @@
+package one.digitalinonovation.dionebank
+
+import java.math.BigDecimal
+
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
+    val salario: BigDecimal
+) : Pessoa(nome, cpf) {
+    abstract fun calculoAuxilio();
+}
--- src/main/kotlin/one/digitalinonovation/dionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinonovation/dionebank/Pessoa.kt
@@ -1,18 +1,6 @@
 package one.digitalinonovation.dionebank
 
-class Pessoa {
-    var nome: String = "Lucas"
-    var cpf: String = "123.123.123-12"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val lucas = Pessoa()
-
-    println(lucas)
-    println(lucas.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
